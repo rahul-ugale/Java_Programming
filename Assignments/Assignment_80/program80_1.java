c9bd66ae7bbd9772f3067953a200753ac0056fc0
Rahul Ugale <rugale980@gmail.com>
2026-03-02 05:32:33 +0530

    Add files via upload

---
+++ Assignments/Assignment_80/program80_1.java
@@ -0,0 +1,119 @@
+import java.util.*;
+
+class program80_1
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter Number of Cities : ");
+        int N = sobj.nextInt();
+
+        if(N <= 0)
+        {
+            System.out.println("Invalid Input");
+            return;
+        }
+
+        int Arr[][] = new int[N][7];
+
+        int i = 0, j = 0;
+        int No = 0;
+
+        System.out.println("Enter Electricity Consumption of Each City : ");
+
+        for(i = 0; i < N; i++)
+        {
+            System.out.println("Enter Consumption of City : "+(i + 1));
+
+            for(j = 0; j < 7; j++)
+            {
+                No = sobj.nextInt();
+
+                if(No < 0)
+                {
+                    System.out.println("Invalid Input");
+                    return;
+                }
+
+                Arr[i][j] = No;
+            }
+        }
+
+        int Weekly[] = new int[7];
+
+        int HighestCity = 0;
+        int MaxWeekly = 0;
+        int PeekDay = 0;
+        int MaxDay_Total = 0;
+        int Sum = 0;
+
+        // Weekly total + highest consumption
+
+        for(i = 0; i < N; i++)
+        {
+            Sum = 0;
+
+            for(j = 0; j < 7; j++)
+            {
+                Sum = Sum + Arr[i][j];
+            }
+
+            Weekly[i] = Sum;
+
+            if(Sum > MaxWeekly)
+            {
+                MaxWeekly = Sum;
+                HighestCity = i;
+            }
+        }
+
+        System.out.println("Highest Consumption : City "+HighestCity);
+        System.out.println("Highest Consumption : "+MaxWeekly);
+
+        int DaySum = 0;
+        int Day = 0;
+
+        // Day with maximum consumption
+
+        for(Day = 0; Day < 7; Day++)
+        {
+            DaySum = 0;
+
+            for(i = 0; i < N; i++)
+            {
+                DaySum = DaySum + Arr[i][Day];
+            }
+
+            if(DaySum > MaxDay_Total)
+            {
+                MaxDay_Total = DaySum;
+                PeekDay = Day;
+            }
+        }
+
+        String DayName[] = {"SUN","MON","TUE","WED","THU","FRI","SAT"};
+
+        System.out.println("Peak Consumption Day : "+PeekDay);
+        System.out.println("Peak Consumption Day Name : "+DayName[PeekDay]);
+
+        System.out.println("Weekly Consumption : ");
+        for(i = 0; i < N; i++)
+        {
+            System.out.println("City : "+(i + 1)+" : "+Weekly[i]);
+        }
+
+        // Cities more than 500 consumption
+        System.out.println("Cities More Than 500 Consumption");
+
+        for(i = 0; i < N; i++)
+        {
+            if(Weekly[i] > 500)
+            {
+                System.out.println("City : "+(i + 1));
+            }
+        }
+
+        sobj.close();
+    }
+}
