d615e9944c4ac5e606ec50aea06ae7e58f5b46be
Rahul Ugale <rugale980@gmail.com>
2026-05-14 12:22:48 +0530

    Add files via upload

---
+++ Bitwise_Operators/program622T.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class program622
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int No = 0;
+        int iDigit = 0;
+        int iCount = 0;
+
+        System.out.println("Enter the Number : ");
+        No = sobj.nextInt();
+
+        while(No != 0)
+        {
+            iDigit = No % 2;
+            iCount = iCount + iDigit;
+            No = No / 2;
+        }
+
+        System.out.print("Number of 1's are : "+iCount);
+    }
+}
+
+/*
+    Decimal             Hexadecimal             Binary
+       0                    0                    0000
+       1                    1                    0001
+       2                    2                    0010
+       3                    3                    0011
+       4                    4                    0100
+       5                    5                    0101
+       6                    6                    0110
+       7                    7                    0111
+       8                    8                    1000
+       9                    9                    1001
+       10                   a                    1010
+       11                   b                    1011
+       12                   c                    1100
+       13                   d                    1101
+       14                   e                    1110
+       15                   f                    1111
+*/
+ 
