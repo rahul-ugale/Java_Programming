ebe37ce1301209168c7048f45cdb90f187779f1d
Rahul Ugale <rugale980@gmail.com>
2026-05-17 22:32:15 +0530

    Add files via upload

---
+++ Bitwise_Operators/program629.java
@@ -0,0 +1,30 @@
+// write the program 15th bit position is ON or OF and mask will be number Hexadecimal 
+
+import java.util.Scanner;
+
+class program629
+{
+    public static void main(String A[])
+    {
+        int iNo = 0,iMask = 0,iResult = 0;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the number : ");
+        iNo = sobj.nextInt();
+
+        iMask = 0x00004000;
+
+        iResult = iNo & iMask;
+
+        if(iResult == iMask)
+        {
+            System.out.println("15th bit is ON");
+        }
+        else
+        {
+            System.out.println("15th bit is OFF");
+        }
+    }
+}
+ 
