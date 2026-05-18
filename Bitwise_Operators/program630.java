07dbcfc8ad80e4bba354dc3e85494b2e8c2177f0
Rahul Ugale <rugale980@gmail.com>
2026-05-18 12:24:34 +0530

    Add files via upload

---
+++ Bitwise_Operators/program630.java
@@ -0,0 +1,30 @@
+// write the program 26th bit position is ON or OF 
+
+import java.util.Scanner;
+
+class program630
+{
+    public static void main(String A[])
+    {
+        int iNo = 0,iMask = 0,iResult = 0;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the number : ");
+        iNo = sobj.nextInt();
+
+        iMask = 0x02000000;
+
+        iResult = iNo & iMask;
+
+        if(iResult == iMask)
+        {
+            System.out.println("26th bit is ON");
+        }
+        else
+        {
+            System.out.println("26th bit is OFF");
+        }
+    }
+}
+ 
